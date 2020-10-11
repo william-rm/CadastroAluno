e2d5e289cb37a379f1167c1da2138f6030eb72e5
William Ribeiro Medeiros <william.rm0410@gmail.com>
2020-10-11 16:35:40 -0300

    Restante da implementação do projeto

--- app/src/main/java/br/usjt/ucsist/cadbasico/LoginActivity.java
+++ app/src/main/java/br/usjt/ucsist/cadbasico/LoginActivity.java
@@ -5,13 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewNovoCadastro;
+    private Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init(this).build();
+
+        textViewNovoCadastro = findViewById(R.id.textViewNovoCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (Hawk.contains("tem_cadastro")){
+            //se tem a propriedade
+            if (Hawk.get("tem_cadastro")){
+                habilitarLogin();
+            }else{
+                desabilitarLogin();
+            }
+        }else {
+            desabilitarLogin();
+        }
+    }
+
+    private void habilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.GONE);
+        buttonLogin.setEnabled(true);
+        buttonLogin.setBackgroundColor(getResources().getColor(R.color.azulEscuro));
+    }
+
+    private void desabilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.VISIBLE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(getColor(R.color.cinza));
     }
 
     public void novoCadastro(View view) {
